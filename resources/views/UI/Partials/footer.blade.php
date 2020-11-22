67290a2aa96cdcb34566b97af4b9de8d1c14e033
FreekAlberti <federicoalberi96@gmail.com>
2020-11-22 15:52:18 +0100

    Added-footer-2

---
+++ resources/views/UI/Partials/footer.blade.php
@@ -0,0 +1,115 @@
+ <footer>
+    <div class="wrapper-footer">
+        <div class="margin-top-100">
+            <div class="flex-box-footer-superiore">
+                <div class="colonna-footer">
+                    <ul>
+                        <li>
+                            <h4 class="titoli-footer">Informazioni</h4>
+                        </li>
+                        <li>
+                            <a href="#">Come funziona BoolBnB</a>
+                        </li>
+                        <li>
+                            <a href="#">Newsroom</a>
+                        </li>
+                        <li>
+                            <a href="#">BoolBnB Plus</a>
+                        </li>
+                        <li>
+                            <a href="#">BoolBnB Luxe</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="colonna-footer">
+                    <ul>
+                        <li>
+                            <h4 class="titoli-footer">Community</h4>
+                        </li>
+                        <li>
+                            <a href="#">Diversit&#224; e appartenenza</a>
+                        </li>
+                        <li>
+                            <a href="#">Accessibilit&#224;</a>
+                        </li>
+                        <li>
+                            <a href="#">BoolBnB Associates</a>
+                        </li>
+                        <li>
+                            <a href="#">Invita gli amici</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="colonna-footer">
+                    <ul>
+                        <li>
+                            <h4 class="titoli-footer">Ospita</h4>
+                        </li>
+                        <li>
+                            <a href="#">Diventa un host</a>
+                        </li>
+                        <li>
+                            <a href="#">Offri un&#39;esperienza OnLine</a>
+                        </li>
+                        <li>
+                            <a href="#">Ospitare responsabilmente</a>
+                        </li>
+                        <li>
+                            <a href="#">Open Homes</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="colonna-footer">
+                    <ul>
+                        <li>
+                            <h4 class="titoli-footer">Assistenza</h4>
+                        </li>
+                        <li>
+                            <a href="#">Centro Assistenza</a>
+                        </li>
+                        <li>
+                            <a href="#">Opzioni di cancellazione</a>
+                        </li>
+                        <li>
+                            <a href="#">Servizio di supporto al vicinato</a>
+                        </li>
+                        <li>
+                            <a href="#">Affidabilit&#224; e sicurezza</a>
+                        </li>
+                    </ul>
+                    <img class="header-bnb-logo" src="{{asset('images/air_bnb_logo.png')}}" alt="logo_airbnb">
+                </div>
+            </div>
+            <hr>
+            <div class="flex-box-footer-inferiore">
+                <div class="flex-box-footer-inferiore-nested">
+                    <ul class="lista-icone-social">
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="flex-box-footer-inferiore-nested">
+                    <small>&#169; 2020 Boolbnb, Inc. All rights reserved</small>
+                </div>        
+            </div>
+        </div>
+    </div>
+</footer>
