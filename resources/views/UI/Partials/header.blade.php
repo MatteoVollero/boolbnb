c8ad3e41b3b9df2303fc8c397a3d6c388b5eff91
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-22 12:36:32 +0100

    added-hero
    boolbnb

--- resources/views/UI/Partials/header.blade.php
+++ resources/views/UI/Partials/header.blade.php
@@ -4,9 +4,20 @@
             <div>
                 <img class="header_bnb_logo" src="{{asset('images/air_bnb_logo.png')}}" alt="logo_airbnb">
             </div>
-            <div>
+            <div class="">
                 <input class="home_search_btn" type="text" placeholder="search for an accomodation">
             </div>
         </nav>
+    </section>
+    <section class="header_main">
+        <img class="hero" src="{{asset('images/Jumbotron_air_bnb.jpg')}}" alt="hero_air_bnb">
+        <div class="header_main_item">
+            <h2 class="intro_text_item">
+                We take you everywhere you want.
+            </h2>
+        </div>
+        <div class="header_main_item"></div>
+        <div class="header_main_item"></div>
+        <div class="header_main_item"></div>
     </section>
 </header>
