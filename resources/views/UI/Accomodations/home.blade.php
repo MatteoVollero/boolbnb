6c9cfbdc4786f7425600fe987e8701b40f7625b7
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-23 14:56:36 +0100

    homepage_first_main_section_structure_done
    boolbnb

--- resources/views/UI/Accomodations/home.blade.php
+++ resources/views/UI/Accomodations/home.blade.php
@@ -6,10 +6,31 @@
 @section('main_content')
 <section class="first_main_section">
     <aside class="aside_accomodations">
+        <div class="aside_wrapper">
+            <div class="aside_item">
 
+            </div>
+            <div class="aside_item">
+
+            </div>
+            <div class="aside_item">
+
+            </div>
+        </div>
     </aside>
     <div class="main_section">
-
+        <div class="first_main_section_wrapper">
+            <div class="accomodation_card">
+                <div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
