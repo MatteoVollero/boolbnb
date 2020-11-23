4072d2f4f6728aa0906d00d6b5496f1f28adb98d
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-23 23:07:24 +0100

    homepage_first_section_aside_main_complete
    boolbnb

--- resources/views/UI/Accomodations/home.blade.php
+++ resources/views/UI/Accomodations/home.blade.php
@@ -4,33 +4,396 @@
     Homepage
 @endsection
 @section('main_content')
+{{-- // main section  --}}
 <section class="first_main_section">
+    {{-- aside --}}
     <aside class="aside_accomodations">
+        {{-- aside wrapper  --}}
         <div class="aside_wrapper">
+            {{-- aside item  --}}
             <div class="aside_item">
-
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
             </div>
+            {{-- aside item  --}}
             <div class="aside_item">
-
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
             </div>
+            {{-- aside item  --}}
             <div class="aside_item">
-
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
             </div>
+            {{-- aside item  --}}
+            <div class="aside_item">
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
+            </div>
+            {{-- aside item  --}}
+            <div class="aside_item">
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
+            </div>
+            {{-- aside item  --}}
+            <div class="aside_item">
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
+            </div>
+            {{-- aside item  --}}
+            <div class="aside_item">
+                {{-- informations item --}}
+                <div class="informations_item">
+                    <span>Country</span>
+                    <span>Region</span>
+                    <span>City</span>
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Price</span>
+                    <span>services</span>
+                </div>
+                {{-- cover image --}}
+                <div class="cover_image">
+                </div>
+            </div>        
         </div>
     </aside>
-    <div class="main_section">
+    {{-- cards section  --}}
+    <div class="cards_section">
+        {{-- first main section wrapper  --}}
         <div class="first_main_section_wrapper">
-            <div class="accomodation_card">
-                <div>
+            {{-- accomodations card --}}
+            <div class="accomodation_cards">
+                {{-- upper cards controller  --}}
+                <div class="upper_cards_scroller">
+                    {{-- card item  --}}
+                    <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                <div class="lower_cards_scroller">
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>
+                     {{-- card item  --}}
+                     <div class="card_item">
+                        {{-- informations item  --}}
+                        <div class="informations_item">
+                            <span>Country</span>
+                            <span>Region</span>
+                            <span>City</span>
+                            <span>Rooms</span>
+                            <span>Beds</span>
+                            <span>Price</span>
+                            <span>services</span>
+                        </div>
+                        {{-- cover image  --}}
+                        <div class="cover_image">
+                        </div>
+                    </div>     
                 </div>
             </div>
         </div>
     </div>
+</div>
 </section>
 @endsection
