e5478e464a904ac71f0319b700b7789a4f1a8675
FreekAlberti <federicoalberi96@gmail.com>
2020-11-24 18:45:00 +0100

    footer completo header wip

--- resources/views/UI/Partials/header.blade.php
+++ resources/views/UI/Partials/header.blade.php
@@ -1,15 +1,23 @@
 <header>
-    <section class="header_top">
-        <nav class="header_nav">
+    <div class="position-flex-header">
+        <div class="wrapper">
             <div>
                 <img class="header_bnb_logo" src="{{asset('images/air_bnb_logo.png')}}" alt="logo_airbnb">
             </div>
-            <div>
+            @if (Route::has('login'))
+                <div class="links">
+                    <a href="#"><i class="fas fa-bars none"></i></a>
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
 
-            </div>
-            <div class="covid_info">
-               <a href="" target="_blank">Looks upon our recent informations about the answer on the COVID19 Emergency</a> 
-            </div>
-        </nav>
-    </section>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </div>
 </header>
