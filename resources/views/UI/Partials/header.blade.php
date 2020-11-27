cb2c22a408e8b09d29d7f2d9e06d065161243093
FreekAlberti <federicoalberi96@gmail.com>
2020-11-27 22:36:48 +0100

    header changed

--- resources/views/UI/Partials/header.blade.php
+++ resources/views/UI/Partials/header.blade.php
@@ -4,20 +4,40 @@
             <div>
                 <img class="header_bnb_logo" src="{{asset('images/air_bnb_logo.png')}}" alt="logo_airbnb">
             </div>
-            @if (Route::has('login'))
-                <div class="links">
-                    <a href="#"><i class="fas fa-bars hamburger"></i></a>
-                    @auth
-                        <a href="{{ url('/') }}" class="link-header">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="link-header">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="link-header">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+             <!-- Right Side Of Navbar -->
+             <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+            
         </div>
     </div>
 </header>
