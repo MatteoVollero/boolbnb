0ee021ba5fa0a0648008ac8a6b97dc2e06cb676e
Domenico Capalbo <capalbodomenico@gmail.com>
2020-11-29 12:29:56 +0100

    Added-client-side-controls-for-register-login-scss-refactory
    boolBnB

--- resources/views/UI/Accomodations/search.blade.php
+++ resources/views/UI/Accomodations/search.blade.php
@@ -1,26 +1,38 @@
 {{-- // Searching accomodations page for the UI --}}
 @extends('UI.layouts.app')
 @section('title')
-    Accomodations
+    Accomodations research
 @endsection
 @section('main_content')
-    <section class="section-search">
-        <div class="elm-responsive-search">
+{{-- section search  --}}
+    <section class="section_search">
+        {{-- elm responsive search  --}}
+        <div class="elm_responsive_search">
             @for ($i = 0; $i < 10; $i++)
-            <div class="elm-search-list">
-                <div class="img-elm-search"></div>
-                <div class="testo-elm-search">
-                    <div class="testo-elm-search-superiore">
-                        <h3 class="title-elm-search">Appartamento</h3>
-                        <p class="text-elm-search">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus fugiat eveniet...<a href="#">Read more</a></p>
-                        <div class="service-elm-searc">
-                            <small class="info-elm-search">3 <i class="fas fa-toilet"></i></small>
-                            <small class="info-elm-search">5 <i class="fas fa-bed"></i></small>
-                            <small class="info-elm-search"><i class="fas fa-wifi"></i></small>
+            {{-- elm search list  --}}
+            <div class="elm_search_list">
+                {{-- img elm search  --}}
+                <div class="img_elm_search"></div>
+                {{-- title elm search  --}}
+                <div class="title_elm_search">
+                    {{-- title elm searc higher  --}}
+                    <div class="title_elm_search_higher">
+                        {{-- title elm search  --}}
+                        <h3 class="title-elm-search">Accomodation</h3>
+                        {{-- text elm search  --}}
+                        <p class="text_elm_search">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus fugiat eveniet...<a href="#">Read more</a></p>
+                        {{-- service elm search  --}}
+                        <div class="service_elm_search">
+                            {{-- info elm search  --}}
+                            <small class="info_elm_search">3 <i class="fas fa-toilet"></i></small>
+                            <small class="info_elm_search">5 <i class="fas fa-bed"></i></small>
+                            <small class="info_elm_search"><i class="fas fa-wifi"></i></small>
                         </div>
                     </div>
-                    <div class="testo-elm-search-inferiore">
-                        <h4 class="price-elm-search">Price</h4>
+                    {{-- text elm search lower  --}}
+                    <div class="text_elm_search_lower">
+                        {{-- price elm search  --}}
+                        <h4 class="price_elm_search">Price</h4>
                     </div>
                 </div>
             </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,38 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+@extends('UI.layouts.app')
+@section('main_content')
+<div class="form_background">
+    <div class="form_wrapper">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form_group">
+                    <label for="email">{{ __('E-Mail Address') }}</label>
+                    <input id="email" type="email" class="form_input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="insert your email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
-            </div>
-        </div>
+                <div class="form_group">
+                    <label for="password">{{ __('Password') }}</label>
+                    <input id="password" type="password" class="form_input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="insert your password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="form_group">
+                    <a href="{{ route('login')}} " class="form_a">
+                        Login
+                    </a>
+                    @if (Route::has('password.request'))
+                        <a id="forgot_password" href="{{ route('password.request') }}">
+                            forgot password?
+                        </a>
+                    @endif
+                </div>
+            </form>
     </div>
 </div>
 @endsection
