245389cfa636c877aed81ba4c50ba97384cd7565
FreekAlberti <federicoalberi96@gmail.com>
2020-11-30 17:52:46 +0100

    main_search changed

--- resources/views/UI/Accomodations/search.blade.php
+++ resources/views/UI/Accomodations/search.blade.php
@@ -5,6 +5,87 @@
 @endsection
 @section('main_content')
     <section class="section-search">
+        {{-- SEARCHBAR --}}
+        <div class="search_bar">
+            <div class="search_item">
+                <label id="City" for="City">City</label><br>
+                <input type="text" id="City" placeholder="Insert the City please">
+            </div>
+            <div class="search_item">
+                <label id="Typology" for="Typology">Typology</label>
+                <div>
+                    <select name="Select" id="Select">
+                        <option value="">
+                            Select
+                        </option> 
+                        <option value="">
+                            House
+                        </option> 
+                        <option value="">
+                            Room
+                        </option>  
+                        <option value="">
+                            Mansion
+                        </option>   
+                    </select>
+                </div>
+            </div>
+            <div class="search_item">
+                <li class="list_menu">
+                    <label id="Distance" for="Distance">Distance</label>
+                    {{-- INSERIRE BARRA GESTIONE DISTANZA --}}
+                </li>
+            </div>
+            <div class="search_item">
+                <li class="list_menu">
+                    <label id="Services" for="Services">Services</label>
+                    <div class="dropleft_menu">
+                        <ul class="flex_items">
+                            <li class="list_item">
+                                <label id="text" for="Beds">Beds</label><br>
+                            </li>
+                            <li class="list_item">
+                                <input type="text" id="Beds" placeholder="Insert the n. of Beds please">
+                            </li>
+                            <li class="list_item">
+                                <label id="Rooms" for="Rooms">Rooms</label>
+                            </li>
+                            <li class="list_item">
+                                <input type="text" id="Rooms" placeholder="Insert the n. of Rooms please">
+                            </li>
+                            <li class="list_item">
+                                <label id="text" for="Services">Services</label>
+                            </li>
+                                <ul class="list_item">
+                                    {{-- @foreach ($services as $service)
+                                    <li class="service_list_item">
+                                        <input type="checkbox" name="service">
+                                        @if ($service->service_name == "wi-fi")
+                                        <i class="fas fa-wifi translate"></i>
+                                        @elseif ($service->service_name == "parking")
+                                        <i class="fas fa-parking translate"></i>
+                                        @elseif ($service->service_name == "pool")
+                                        <i class="fas fa-swimmer translate"></i>
+                                        @elseif ($service->service_name == "reception")
+                                        <i class="fas fa-concierge-bell translate"></i>
+                                        @elseif ($service->service_name == "sauna")
+                                        <i class="fas fa-hot-tub translate"></i>
+                                        @elseif ($service->service_name == "sea_view")
+                                        <i class="fas fa-water translate"></i>
+                                        @endif
+                                    </li>
+                                    @endforeach --}}
+                                </ul>
+                            </li> 
+                        </ul>
+                    </div>
+                </li>
+            </div>
+            <div class="btn_search">
+                <button type="submit">Search<i class="fas fa-search"></i></button>
+            </div>
+        </div>
+        {{-- RESULTS --}}
         <div class="elm-responsive-search">
             @for ($i = 0; $i < 10; $i++)
             <div class="elm-search-list">
