7c42437529138658bde3293c51cc62bf8ea17936
Domenico Capalbo <capalbodomenico@gmail.com>
2020-12-02 19:16:31 +0100

    jumbotron-show-ui-refactory
    boolBnB

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 require('./bootstrap');
 require('./mobile-or-tablet'); // per gestione map Tom Tom
 require('././Partials/functions.js');
+require('././Partials/map.js');
 const Handlebars = require("handlebars");
