42e1ff34f4f8f4b3c57a4e19c18b2a35de86ae9c
Domenico Capalbo <capalbodomenico@gmail.com>
2020-12-02 16:09:52 +0100

    comments-added
    boolBnB

--- resources/views/UPRA/Accomodations/show.blade.php
+++ resources/views/UPRA/Accomodations/show.blade.php
@@ -1,5 +1,6 @@
 {{-- // showing the single accomodations for the UPRA --}}
 @extends('UPRA.layouts.app')
+{{-- link head --}}
 @section('chart_content')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.js"></script>
 @endsection
